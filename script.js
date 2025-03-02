5a2e63c1c7e2b4ec8e5ca4c76c5d7e01fad424dc
DK <don@promptnow.com>
2025-03-02 21:42:43 +0700

    Update validating condition

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 setTimeout(() => {
     let input = prompt("fill number");
-    if (input === null) {
-        alert("Invalid input.");
-        console.log("Invalid input.");
+    if (!input) {
+        // check if falsy
     } else if (input % 2 === 0) {
         alert("even");
         console.log("even");
         document.getElementsByTagName("p")[0].innerHTML = "even";
-    } else {
-        alert("Invalid input.");
-        console.log("Invalid input.");
     }
 }, 400);
