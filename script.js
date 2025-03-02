26ca26434b492dcc3eeea3314540387c6b6b6b89
DK <don@promptnow.com>
2025-03-02 21:35:48 +0700

    Implement input validation

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 setTimeout(() => {
     let input = prompt("fill number");
-    if (input % 2 === 0) {
+    if (input === null) {
+        alert("Invalid input.");
+        console.log("Invalid input.");
+    } else if (input % 2 === 0) {
         alert("even");
         console.log("even");
         document.getElementsByTagName("p")[0].innerHTML = "even";
+    } else {
+        alert("Invalid input.");
+        console.log("Invalid input.");
     }
 }, 400);
